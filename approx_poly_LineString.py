faba5118c595f3a647583661e685398ffb2f81a4
theViz343 <vishwesh103@gmail.com>
2020-04-18 21:10:14 +0530

    Added clustering and made file PEP8 compliant

--- approx_poly_LineString.py
+++ approx_poly_LineString.py
@@ -2,95 +2,141 @@
 # using the convex hull
 # functions of the Shapely library.
 # A map of districts of Maharashtra is used for this example.
-from math import ceil
-import matplotlib.pyplot as plt
-import numpy as np
 import geopandas
 import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
 from geopandas import GeoSeries
-from shapely.geometry import Polygon,Point,MultiLineString
+from shapely.geometry import Point, MultiLineString, MultiPoint
+from sklearn.cluster import KMeans
 
 
-def squareInside(point, width, poly):
-    x=point.x
-    y=point.y
+def squareInside(point, width, poly) :
+    x = point.x
+    y = point.y
     # - -
     # 0 -
-    if point.within(poly):
+    if point.within( poly ) :
         return True
 
     # - -
     # - 0
-    point=Point(x+width, y)
-    if point.within(poly):
+    point = Point( x + width, y )
+    if point.within( poly ) :
         return True
 
     # - 0
     # - -
-    point=Point(x+width, y+width)
-    if point.within(poly):
+    point = Point( x + width, y + width )
+    if point.within( poly ) :
         return True
 
     # 0 -
     # - -
-    point = Point(x, y+width)
-    if point.within(poly):
+    point = Point( x, y + width )
+    if point.within( poly ) :
         return True
 
-    return False # No corner is inside polygon
+    return False  # No corner is inside polygon
 
-g=geopandas.read_file("maharashtra/maharashtra_administrative.shp")
-# Three subplots showing different Info
-fig,ax=plt.subplots(2,2)
 
-g.plot(ax=ax[0][0])    # Plot shapefile info directly
+### Parameters
+maha_shape = geopandas.read_file( "maharashtra/maharashtra_administrative.shp" )
+width = 0.4
+offset = width / 2
+noOfClusters = 3
+###
 
 # Calculate the Convex Hull of all MultiLineString districts
-boundary=[]
-for district in g["geometry"]:
-    boundary.append(district)
-boundary_poly=MultiLineString(boundary).convex_hull
-area=GeoSeries(boundary_poly)
+boundary = []
+for district in maha_shape["geometry"] :
+    boundary.append( district )
+
+boundary_poly = MultiLineString( boundary ).convex_hull
+area = GeoSeries( boundary_poly )
 
 # Calculation of points with set width between them
-width=0.4
-offset=width/2
-x0=area.bounds["minx"][0]-width
-x1=area.bounds["maxx"][0]+width
-y0=area.bounds["miny"][0]-width
-y1=area.bounds["maxy"][0]+width
-boundaryPointsx=[x0,x0,x1,x1,x0]
-boundaryPointsy=[y0,y1,y1,y0,y0]
-ax[0][0].plot(boundaryPointsx, boundaryPointsy, color='g')
-ax[0][1].plot(boundaryPointsx, boundaryPointsy, color='g')
-ax[1][0].plot(boundaryPointsx, boundaryPointsy, color='g')
-points=[]
-for i in np.arange(x0,x1,width):
-    for j in np.arange(y0,y1,width):
-        points.append((i,j))
 
-pointsgeom=[]
-pointsgeomshifted=[]
-for point in points:
-    if squareInside(Point(point),width,boundary_poly):
+x0 = area.bounds["minx"][0] - width
+x1 = area.bounds["maxx"][0] + width
+y0 = area.bounds["miny"][0] - width
+y1 = area.bounds["maxy"][0] + width
+boundaryPointsx = [x0, x0, x1, x1, x0]
+boundaryPointsy = [y0, y1, y1, y0, y0]
+
+# Calculating point coordinates across the whole boundary
+points = []
+for i in np.arange( x0, x1, width ) :
+    for j in np.arange( y0, y1, width ) :
+        points.append( (i, j) )
+
+pointsgeom = []  # normal points python list
+pointsgeomshifted = []  # shifted (to the centre of cell boundary) point python list
+GridPoints = []  # points numpy array
+
+# Calculation of points within the area
+for point in points :
+    if squareInside( Point( point ), width, boundary_poly ) :
+        GridPoints.append( point )
         pointsgeom.append( Point( point ) )
-        point=(point[0]+offset,point[1]+offset)
-        pointsgeomshifted.append(Point(point))
+        point = (point[0] + offset, point[1] + offset)
+        pointsgeomshifted.append( Point( point ) )
 
-#Plots
+npGridPoints = np.array( GridPoints )
 
-# Centre-shifted point plot
-area.plot(ax=ax[0][1])
-p=GeoSeries(pointsgeomshifted)
-p.plot(ax=ax[0][1],color="red",markersize=1)
+# Clustering of area into different sections
+clusteredData = KMeans( n_clusters=noOfClusters ).fit( npGridPoints )
+clusters = np.array( [] )
+centroids = clusteredData.cluster_centers_
+labels = clusteredData.labels_  # labels tell the cluster to which a given point corresponds to
+collection = []
+for i in range( noOfClusters ) :
+    collection.append( npGridPoints[labels == i] )
 
-# Raw points and cell boundary plot
-area.plot(ax=ax[1][0])
-q=GeoSeries(pointsgeom)
-q.plot(ax=ax[1][0],color="red",markersize=1)
-for point in pointsgeom:
-    rect=patches.Rectangle((point.x,point.y),width,width,linewidth=0.5,edgecolor='r',facecolor='none')
-    ax[1][0].add_patch(rect)
+clusters = np.append( clusters, collection, axis=0 )
 
+# Plots
+fig, ax = plt.subplots( 2, 2 )
 
+# Boundaries of the plots so that they scale equally (might be unnecessary)
+ax[0][0].plot( boundaryPointsx, boundaryPointsy, color='g' )
+ax[0][1].plot( boundaryPointsx, boundaryPointsy, color='g' )
+ax[1][0].plot( boundaryPointsx, boundaryPointsy, color='g' )
+ax[1][1].plot( boundaryPointsx, boundaryPointsy, color='g' )
+
+# PLOT1: Plot shapefile info directly
+ax[0][0].set_title('Shapefile')
+maha_shape.plot( ax=ax[0][0] )
+
+# PLOT2: Centre-shifted point plot
+ax[0][1].set_title('Centre-shifted points')
+area.plot( ax=ax[0][1], facecolor='none', edgecolor='blue' )
+p = GeoSeries( pointsgeomshifted )
+p.plot( ax=ax[0][1], color="red", markersize=1 )
+
+# PLOT3: Raw points and cell boundary plot
+ax[1][0].set_title('Cell boundary plot')
+area.plot( ax=ax[1][0], facecolor='none', edgecolor='blue' )
+q = GeoSeries( pointsgeom )
+q.plot( ax=ax[1][0], color="red", markersize=1 )
+for point in pointsgeom :
+    rect = patches.Rectangle( (point.x, point.y), width, width, linewidth=1, edgecolor='r', facecolor='none' )
+    ax[1][0].add_patch( rect )
+
+# PLOT4: Colored clustered plot with cell boundaries
+ax[1][1].set_title('Clustered plot')
+colors = ['red', 'black', 'green', 'yellow', 'orange', 'brown']
+area.plot( ax=ax[1][1], facecolor='none', edgecolor='blue' )
+
+geocentroids = GeoSeries( MultiPoint( centroids ) )
+geocentroids.plot( ax=ax[1][1], color=colors[-1], markersize=50 )
+
+for i in range( noOfClusters ) :
+    geocluster = GeoSeries( MultiPoint( clusters[i] ) )
+    geocluster.plot( ax=ax[1][1], color=colors[i], markersize=1 )  # printing cluster points
+    for point in MultiPoint( clusters[i] ) :
+        rect = patches.Rectangle( (point.x, point.y), width, width, linewidth=1, edgecolor=colors[i],
+                                  facecolor='none' )
+        ax[1][1].add_patch( rect )  # printing cell boundary
+    print( 'Size of cluster', i, "is", clusters[i].size )
 plt.show()
